b80a92087085420c6ddea2fa9e099003c0539c2d
ssandstr <sandstrom.shaun@gmail.com>
2019-09-04 12:35:46 -0700

    Initial dump, app structure like original, but using .env file instead of a config file

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+ 
+const app = express();
+
+// Bodyparser Middleware
+app.use(express.json());
+
+// DB Config
+const db = process.env.mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
